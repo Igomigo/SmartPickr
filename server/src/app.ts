e89bbdba7f3f20b1ca1a4d11b9baac160129ecd4
Igomigo <igomigofatai@gmail.com>
2025-08-30 14:52:01 +0100

    Enhance server configuration: add middleware for CORS, Helmet, and Morgan; update package dependencies for improved security and logging

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,14 @@ import * as dotenv from "dotenv";
 import express, { Express } from "express";
 import { EnvGuard } from "env-preflight"; // I wrote this package☺️, find it on https://github.com/Igomigo/env-preflight
 import { envRule } from "./utils/env_rule";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+
+// Middleware
 
 // Routes
-import productRoutes from "./routes/product.routes"
+import productRoutes from "./routes/product.routes";
 
 dotenv.config();
 export class App {
@@ -16,7 +21,15 @@ export class App {
     this.setupEnvGuard();
     this.PORT = Number(process.env.PORT);
     this.app = express();
+    this.setUpMiddlewares();
     this.setUproutes();
+  }
+
+  private setUpMiddlewares() {
+    this.app.use(helmet());
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(morgan("dev"));
   }
 
   private setUproutes() {
@@ -31,7 +44,7 @@ export class App {
   public listen() {
     this.app.listen(this.PORT, () => {
       console.log(
-        `SmartPickr server listening on http://localhost:${this.PORT}`
+        `🧠🔎SmartPickr server listening on http://localhost:${this.PORT}`
       );
     });
   }
--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -5,7 +5,8 @@ class ProductAnalysisController {
 
   public async analyzeProduct(req: Request, res: Response) {
     try {
-      console.log("Analyzing product...");
+      const { url }: { url: string } = req.body;
+      console.log("Analyzing product...", url);
     } catch (error) {
       console.error("Error analyzing product:", error);
       res.status(500).json({ error: "Failed to analyze product" });
