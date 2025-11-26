aba4233288686a947483a8ca601e082a3a4c7259
Igomigo <igomigofatai@gmail.com>
2025-11-26 13:23:59 +0100

    Add product details extraction in Jiji scraper: Include product images, title, price, and specifications in the returned details.

--- server/src/scrapers/jiji/helpers/getProductDetails.ts
+++ server/src/scrapers/jiji/helpers/getProductDetails.ts
@@ -1,10 +1,26 @@
 import { CheerioAPI } from "crawlee";
 import { logger } from "../../../utils/logger";
+import { getProductSpecs } from "./getProductSpecs";
 
 export const getProductDetails = async ($: CheerioAPI) => {
   try {
+    // Get the product image(s)
+    const productImages = $("img.b-advert-image-list__image").map((_, element) => {
+      return $(element).attr("src")?.trim() || "";
+    }).get() || [];
+    // Get the product title
     const productTitle = $(".b-advert-title-inner").text().trim();
+    // Get the product price
     const productPrice = $(".qa-advert-price-view-value").text().trim();
+    // Get the product specs
+    const productSpecs = await getProductSpecs($);
+    // Return the product details
+    return {
+      productImages,
+      productTitle,
+      productPrice,
+      productSpecs,
+    };
   } catch (error) {
     logger.error(`Error getting product details: ${(error as Error).message}`);
     throw error;
---
+++ server/src/scrapers/jiji/helpers/getProductSpecs.ts
@@ -0,0 +1,43 @@
+import { CheerioAPI } from "crawlee";
+import { logger } from "../../../utils/logger";
+
+/**
+ * Get the detailed product specs from the product page
+ * @param $ - The CheerioAPI instance
+ * @returns {Promise<Record<string, string>>} The product specs
+ * @description
+ * This function gets the detailed product specs from the product page.
+ * The product specs are stored in a record of key-value pairs.
+ */
+export const getProductSpecs = async ($: CheerioAPI) => {
+  try {
+    const productSpecs: Record<string, string> = {};
+    // Get initial product specs
+    $(".b-advert-item-details-collapser__visible div.b-advert-attribute").each(
+      (_, element) => {
+        const key = $(element).find(".b-advert-attribute__key").text().trim();
+        const value = $(element)
+          .find(".b-advert-attribute__value")
+          .text()
+          .trim();
+        productSpecs[key] = value;
+      }
+    );
+
+    // Get additional product specs if they exist
+    $(
+      ".b-advert-item-details-collapser__rest-wrapper div.b-advert-attribute"
+    ).each((_, element) => {
+      const key = $(element).find(".b-advert-attribute__key").text().trim();
+      const value = $(element).find(".b-advert-attribute__value").text().trim();
+      productSpecs[key] = value;
+    });
+
+    return productSpecs;
+  } catch (error) {
+    logger.error(
+      `Error getting detailed product specs: ${(error as Error).message}`
+    );
+    throw error;
+  }
+};
