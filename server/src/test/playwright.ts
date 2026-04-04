f5dbbc2f25770e892245914a6ac6de25fd4544f1
Fatai <igomigo@gmail.com>
2026-04-04 09:01:47 +0100

    Implement structural updates and optimizations across multiple modules

--- server/src/test/playwright.ts
+++ server/src/test/playwright.ts
@@ -8,12 +8,12 @@ import jumia from "../scrapers/jumia";
 const url =
   "https://www.jumia.com.ng/fashion-cooperate-business-mens-leather-luxury-wedding-office-formal-shoes-black-226547771.html";
 
-//const searchTerm = "Men shoes";
+const searchTerm = "Women Bags";
 
 const scrapeProduct = async () => {
-  //await jiji.scrapeProductsBySearch({ searchTerm });
+  await jiji.scrapeProductsBySearch({ searchTerm });
   //await jumia.scrapeSearchResults({ searchTerm });
-  await jumia.scrapeProductPage(url);
+  //await jumia.scrapeProductPage(url);
 };
 
 scrapeProduct();
