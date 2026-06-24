5377c19370c2b687217de3abd60cbb435a424cfe
Fatai <igomigo@gmail.com>
2026-06-24 18:49:26 +0100

    fix: update environment variable key from X_CUSTOM_TOKEN to X-CUSTOM-TOKEN for consistency

--- server/src/utils/env_rule.ts
+++ server/src/utils/env_rule.ts
@@ -3,6 +3,6 @@ import { EnvGuardConfig } from "env-preflight";
 export const envRule: EnvGuardConfig = {
   required: {
     PORT: "number",
-    X_CUSTOM_TOKEN: "string",
+    "X-CUSTOM-TOKEN": "string",
   },
 };
